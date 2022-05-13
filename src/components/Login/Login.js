1b23bf4ce91ea53b186829f414ca47822441c0b5
Tarin Nuttee <zzc0427895@celestica.com>
2022-05-13 09:50:18 +0700

    commit onm aster

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -65,12 +65,12 @@ const Login = (props) => {
   };
 
   function onSuccess(googleUser) {
-    console.log(googleUser);
-    // var profile = googleUser.getBasicProfile();
-    // console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    // console.log("Name: " + profile.getName());
-    // console.log("Image URL: " + profile.getImageUrl());
-    // console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+    // console.log(googleUser);
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log("Name: " + profile.getName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
 
   return (
